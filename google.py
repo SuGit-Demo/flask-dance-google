50a65e32694a91d70a2b8a859668fe856a9dc9be
SuGit-Demo <89434151+SuGit-Demo@users.noreply.github.com>
2022-06-08 09:28:46 +0800

    Update google.py

--- google.py
+++ google.py
@@ -10,9 +10,28 @@ google_bp = make_google_blueprint(scope=["profile", "email"])
 app.register_blueprint(google_bp, url_prefix="/login")
 
 @app.route("/")
+@app.route('/')
 def index():
-    if not google.authorized:
-        return redirect(url_for("google.login"))
-    resp = google.get("/oauth2/v1/userinfo")
+    return render_template("home.html")
+
+@app.route('/welcome')
+def welcome():
+    resp = google.get("/oauth2/v2/userinfo")
     assert resp.ok, resp.text
-    return "You are {email} on Google".format(email=resp.json()["email"])
+    email=resp.json()["email"]
+
+    return render_template("welcome.html",email=email)
+
+@app.route("/login/google")
+def login():
+    if not google.authorized:
+        return render_template(url_for("google.login"))
+
+    resp = google.get("/oauth2/v2/userinfo")
+    assert resp.ok, resp.text
+    email=resp.json()["email"]
+
+    return render_template("welcome.html",email=email)
+
+if __name__ == "__main__":
+    app.run()
